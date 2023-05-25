83d8699a7dd00f7450fba7fce8f6cb20d86829d9
sindre-kodehode <sindre.kodehode@gmail.com>
2023-05-25 15:17:39 +0200

    add event folder and handler

--- bot.js
+++ bot.js
@@ -1,81 +1,74 @@
+//------------------------------------------------------------------------------
 // imports
+//------------------------------------------------------------------------------
 import dotenv from "dotenv";
 import fs     from "fs/promises";
 import path   from "node:path";
 
+//------------------------------------------------------------------------------
 // named imports
+//------------------------------------------------------------------------------
 import { Client            } from "discord.js";
 import { Collection        } from "discord.js";
-import { Events            } from "discord.js";
 import { GatewayIntentBits } from "discord.js";
 
-// initialize environment variables
+//------------------------------------------------------------------------------
+// load environment variables
+//------------------------------------------------------------------------------
 dotenv.config();
 const { DISCORD_TOKEN } = process.env;
+const __filename        = new URL( import.meta.url ).pathname;
+const __dirname         = path.dirname( __filename );
 
+//------------------------------------------------------------------------------
+// initialize a new client
+//------------------------------------------------------------------------------
 const client = new Client({
   intents : [ GatewayIntentBits.Guilds ]
 });
 
-client.commands = new Collection();
-
-const __filename   = new URL( import.meta.url ).pathname;
-const __dirname    = path.dirname( __filename );
-const commandsPath = path.join( __dirname, "commands" );
+//------------------------------------------------------------------------------
+// dynamically load commands from a folder
+//------------------------------------------------------------------------------
+client.commands      = new Collection();
+const commandsFolder = path.join( __dirname, "commands" );
 
 const commandFiles = ( await fs
-  .readdir( commandsPath ) )
+  .readdir( commandsFolder ) )
   .filter( fileName => fileName.endsWith( ".js" ) );
 
-// load all commandfiles from the command folder
-for ( const fileName of commandFiles ) {
-  const filePath = path.join( commandsPath, fileName );
+commandFiles.forEach( async file => {
+  const filePath = path.join( commandsFolder, file );
   const command  = await import( filePath );
   
-  if ( "data" in command && "execute" in command )
+  if ( command[ "data" ] && command[ "execute" ] )
     client.commands.set( command.data.name, command );
   else
     console.warn(
-      `[WARNING] The command at ${ filePath } is missing a required "data" or "execute" property.`
+      `[WARNING] The command at ${
+        filePath
+       } is missing a required "data" or "execute" property.`
     );
-}
-
-// command handler
-client.on( Events.InteractionCreate, async interaction => {
-  if ( !interaction.isChatInputCommand() ) return;
-
-  const { commandName } = interaction;
-  const command         = interaction.client.commands.get( commandName );
-
-  if ( !command ) {
-    console.error( `No command matching ${ commandName } was found.` );
-    return;
-  }
-
-  try {
-    await command.execute( interaction );
-  }
-
-  catch ( error ) {
-    console.error( error );
-
-    if ( interaction.replied || interaction.deferred )
-      await interaction.followUp({
-      content   : "There was an error while executing this command!",
-      ephemeral : true
-    });
-
-    else
-      await interaction.reply({
-      content   : "There was an error while executing this command!",
-      ephemeral : true
-    });
-
-  }
 });
 
-client.once( Events.ClientReady, client => {
-  console.log( `Ready! Logged in as ${ client.user.tag }.` );
+//------------------------------------------------------------------------------
+// handle events
+//------------------------------------------------------------------------------
+const eventsFolder = path.join( __dirname, "events" );
+const eventFiles = ( await fs
+  .readdir( eventsFolder ) )
+  .filter( fileName => fileName.endsWith( ".js" ) );
+
+eventFiles.forEach( async file => {
+  const filePath = path.join( eventsFolder, file );
+  const event    = await import( filePath );
+
+  client[ event.once ? "once" : "on" ]( event.name, ( ...args ) => 
+    event.execute( ...args )
+  );
 });
 
+//------------------------------------------------------------------------------
+// start client
+//------------------------------------------------------------------------------
 client.login( DISCORD_TOKEN );
---
+++ events/interactionCreate.js
@@ -0,0 +1,38 @@
+import { Events } from "discord.js";
+
+const name    = Events.InteractionCreate;
+const execute = async interaction => {
+  if ( !interaction.isChatInputCommand() ) return;
+
+  const { commandName } = interaction;
+  const command         = interaction.client.commands.get( commandName );
+
+  if ( !command ) {
+    console.error( `No command matching ${ commandName } was found.` );
+    return;
+  }
+
+  try {
+    await command.execute( interaction );
+  }
+
+  catch ( error ) {
+    console.error( `[ERROR] Error executing ${ commandName }`);
+    console.error( error );
+
+    if ( interaction.replied || interaction.deferred )
+      await interaction.followUp({
+        content   : "There was an error while executing this command!",
+        ephemeral : true
+    });
+
+    else
+      await interaction.reply({
+        content   : "There was an error while executing this command!",
+        ephemeral : true
+    });
+
+  }
+};
+
+export { name, execute };
